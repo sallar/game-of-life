9cd020f605874dfae5670283370c82bea258487b
Sallar Kaboli <sallar.kaboli@gmail.com>
2018-05-04 16:42:45 +0300

    Change indent

--- index.js
+++ index.js
@@ -17,46 +17,46 @@ const height = universe.height();
 let animationFrame = null;
 
 const fps = new class {
-  constructor() {
-    this.fps = document.getElementById("fps");
-    this.frames = [];
-    this.lastFrameTimeStamp = performance.now();
-  }
-
-  render() {
-    // Convert the delta time since the last frame render into a measure
-    // of frames per second.
-    const now = performance.now();
-    const delta = now - this.lastFrameTimeStamp;
-    this.lastFrameTimeStamp = now;
-    const fps = 1 / delta * 1000;
-
-    // Save only the latest 100 timings.
-    this.frames.push(fps);
-    if (this.frames.length > 100) {
-      this.frames.shift();
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
     }
 
-    // Find the max, min, and mean of our 100 latest timings.
-    let min = Infinity;
-    let max = -Infinity;
-    let sum = 0;
-    for (let i = 0; i < this.frames.length; i++) {
-      sum += this.frames[i];
-      min = Math.min(this.frames[i], min);
-      max = Math.max(this.frames[i], max);
-    }
-    let mean = sum / this.frames.length;
+    render() {
+        // Convert the delta time since the last frame render into a measure
+        // of frames per second.
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
 
-    // Render the statistics.
-    this.fps.textContent = `
+        // Save only the latest 100 timings.
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+        // Render the statistics.
+        this.fps.textContent = `
   Frames per Second:
            latest = ${Math.round(fps)}
   avg of last 100 = ${Math.round(mean)}
   min of last 100 = ${Math.round(min)}
   max of last 100 = ${Math.round(max)}
   `.trim();
-  }
+    }
 }();
 
 // Give the canvas room for all of our cells and a 1px border
@@ -68,106 +68,106 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 const ctx = canvas.getContext("2d");
 
 const drawGrid = () => {
-  ctx.beginPath();
-  ctx.lineWidth = 1;
-  //   ctx.lineWidth = 1 / window.devicePixelRatio;
-  ctx.strokeStyle = GRID_COLOR;
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    //   ctx.lineWidth = 1 / window.devicePixelRatio;
+    ctx.strokeStyle = GRID_COLOR;
 
-  // Vertical lines.
-  for (let i = 0; i <= width; i++) {
-    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
-    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
-  }
+    // Vertical lines.
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
 
-  // Horizontal lines.
-  for (let j = 0; j <= height; j++) {
-    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
-    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
-  }
+    // Horizontal lines.
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
 
-  ctx.stroke();
+    ctx.stroke();
 };
 
 const getIndex = (row, column) => {
-  return row * width + column;
+    return row * width + column;
 };
 
 const drawCells = () => {
-  const cellsPtr = universe.cells();
-  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
 
-  ctx.beginPath();
+    ctx.beginPath();
 
-  for (let row = 0; row < height; row++) {
-    for (let col = 0; col < width; col++) {
-      const idx = getIndex(row, col);
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
 
-      ctx.fillStyle = cells[idx] === DEAD ? DEAD_COLOR : ALIVE_COLOR;
+            ctx.fillStyle = cells[idx] === DEAD ? DEAD_COLOR : ALIVE_COLOR;
 
-      ctx.fillRect(
-        col * (CELL_SIZE + 1) + 1,
-        row * (CELL_SIZE + 1) + 1,
-        CELL_SIZE,
-        CELL_SIZE
-      );
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
     }
-  }
 
-  ctx.stroke();
+    ctx.stroke();
 };
 
 canvas.addEventListener("click", event => {
-  const boundingRect = canvas.getBoundingClientRect();
+    const boundingRect = canvas.getBoundingClientRect();
 
-  const scaleX = canvas.width / boundingRect.width;
-  const scaleY = canvas.height / boundingRect.height;
+    const scaleX = canvas.width / boundingRect.width;
+    const scaleY = canvas.height / boundingRect.height;
 
-  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
-  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+    const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+    const canvasTop = (event.clientY - boundingRect.top) * scaleY;
 
-  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
-  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+    const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+    const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
 
-  universe.toggle_cell(row, col);
+    universe.toggle_cell(row, col);
 
-  drawCells();
-  drawGrid();
+    drawCells();
+    drawGrid();
 });
 
 const renderLoop = () => {
-  universe.tick();
+    universe.tick();
 
-  drawGrid();
-  drawCells();
+    drawGrid();
+    drawCells();
 
-  fps.render();
+    fps.render();
 
-  animationFrame = requestAnimationFrame(renderLoop);
+    animationFrame = requestAnimationFrame(renderLoop);
 };
 
 const isPaused = () => {
-  return animationFrame === null;
+    return animationFrame === null;
 };
 
 const playPauseButton = document.getElementById("play-pause");
 
 const play = () => {
-  playPauseButton.textContent = "⏸";
-  renderLoop();
+    playPauseButton.textContent = "⏸";
+    renderLoop();
 };
 
 const pause = () => {
-  playPauseButton.textContent = "▶";
-  cancelAnimationFrame(animationFrame);
-  animationFrame = null;
+    playPauseButton.textContent = "▶";
+    cancelAnimationFrame(animationFrame);
+    animationFrame = null;
 };
 
 playPauseButton.addEventListener("click", event => {
-  if (isPaused()) {
-    play();
-  } else {
-    pause();
-  }
+    if (isPaused()) {
+        play();
+    } else {
+        pause();
+    }
 });
 
 play();
